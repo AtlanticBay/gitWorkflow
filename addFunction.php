e79c993a9e4680a5cff23cc476b4c279d002adfb
Corey Shelton <coreyds@gmail.com>
2016-04-11 10:22:08 -0400

    Added calculate function

---
+++ addFunction.php
@@ -0,0 +1,5 @@
+<?php 
+
+function addValues($a, $b) {
+	return $a + $b;
+}
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+require_once 'addFunction.php';
 
 echo "Welcome to GitHub Workflow Training!!!\n";
+
+echo "Calculate 5 + 5 = " . addValues(5, 5);
+
 
 ?>
